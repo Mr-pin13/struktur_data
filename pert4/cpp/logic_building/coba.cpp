dc4ea748dbd984c0312294b99273169ccff114ce
root <yah411072@gmail.com>
2025-04-30 16:19:45 +0700

    struktur dataaaaaa

---
+++ pert4/cpp/logic_building/coba.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void tambah (int a, int b){
+    cout << "Hasil Tambah: " << a + b << endl;
+}
+
+void kurang (int a, int b){
+    cout << "Hasil Kurang: " << a - b << endl;
+}
+
+void kali ( int a, int b) {
+    cout << "Hasil Kali: " << a * b << endl;
+}
+
+void bagi (int a, int b) {
+    cout << "Hasil Bagi: " << a / b << endl;
+}
+
+int main (){
+    int a, b;
+    cout << "Masukkan Angka: " << endl;
+    cin >> a >> b;
+
+    tambah (a, b);
+    kurang (a, b);
+    kali (a, b);
+    bagi (a, b);
+    return 0;
+}
